c65941d26874fc58e7fad5b4508d972922ace260
yndyk <suivivor0519@gmail.com>
2021-04-15 13:45:25 +0900

    //スクリプトを保存する

---
+++ Unity/前期制作/Assets/Sprect/PlayerContoll.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoll : MonoBehaviour
+{
+    public float speed;
+    Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent("Animator") as Animator;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalKey = Input.GetAxis("Horizontal");
+
+        if( Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(speed, 0, 0);
+            animator.SetBool("warposwoik", true);
+        }
+        else
+        {
+            animator.SetBool("warposwoik", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-speed, 0, 0);
+            animator.SetBool("warposwoik", true);
+        }
+        
+    }
+
+  
+}
