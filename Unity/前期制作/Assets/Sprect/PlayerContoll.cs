8e75e1c07275688d17fdd4d5c664016aa73da87a
yndyk <suivivor0519@gmail.com>
2021-04-15 15:08:39 +0900

    //変更しました。

--- Unity/前期制作/Assets/Sprect/PlayerContoll.cs
+++ Unity/前期制作/Assets/Sprect/PlayerContoll.cs
@@ -6,12 +6,16 @@ public class PlayerContoll : MonoBehaviour
 {
     public float speed;
     Animator animator;
+    public float flap = 100f;
+    Rigidbody2D rd2D;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent("Animator") as Animator;
+        rd2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,6 +23,7 @@ public class PlayerContoll : MonoBehaviour
     {
         float horizontalKey = Input.GetAxis("Horizontal");
 
+        //移動
         if( Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += new Vector3(speed, 0, 0);
@@ -34,7 +39,25 @@ public class PlayerContoll : MonoBehaviour
             transform.position += new Vector3(-speed, 0, 0);
             animator.SetBool("warposwoik", true);
         }
-        
+        //攻撃
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("Attack", true);
+        }
+        else 
+        {
+            animator.SetBool("Attack", false);
+        }
+        //ジャンプ
+        if (Input.GetKeyDown("j")) 
+        {
+            rd2D.AddForce(Vector2.up * flap);
+            animator.SetBool("Jump", true);
+        }
+        else 
+        {
+            animator.SetBool("Jump", false);
+        }
     }
 
   
