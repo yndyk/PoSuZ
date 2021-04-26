11695f1fe0fdc959824ec821eecebab0c4ce3fe3
TakeshiOki <sinonomeiruka@outlook.jp>
2021-04-26 15:12:11 +0900

    //変更

--- Unity/前期制作/Assets/Demo Oki/Scripts/player2.cs
+++ Unity/前期制作/Assets/Demo Oki/Scripts/player2.cs
@@ -5,161 +5,58 @@ using UnityEngine;
 public class player2 : MonoBehaviour
 {
     #region//インスペクターで設定する
-    [Header("移動速度")] public float speed;
-    [Header("重力")] public float gravity;
-    [Header("ジャンプ速度")] public float jumpSpeed;
-    [Header("ジャンプする高さ")] public float jumpHeight;
-    [Header("ジャンプする長さ")] public float jumpLimitTime;
-    [Header("接地判定")] public GroundCheck ground;
-    [Header("天井判定")] public GroundCheck head;
-    [Header("ダッシュの速さ表現")] public AnimationCurve dashCurve;
-    [Header("ジャンプの速さ表現")] public AnimationCurve jumpCurve;
+    [Header("移動速度")] public float inputSpeed;
+    [Header("ジャンプ力")] public float jumpPower;
     #endregion
 
     #region//プライベート変数
-    private Animator anim = null;
-    private Rigidbody2D rb = null;
-    private bool isGround = false;
-    private bool isJump = false;
-    private bool isRun = false;
-    private bool isHead = false;
-    private float jumpPos = 0.0f;
-    private float dashTime = 0.0f;
-    private float jumpTime = 0.0f;
-    private float beforeKey = 0.0f;
+    private Rigidbody2D rb2d;
+    private float x_val;
+    private float speed;
     #endregion
 
     private void Start()
     {
-        //コンポーネントのインスタンスを取得する
-        anim = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        x_val = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown("space"))
+        {
+            rb2d.AddForce(Vector2.up * jumpPower);
+        }
+
+        Vector3 left_SP = transform.position - Vector3.right * 0.3f;
+        Vector3 right_SP = transform.position + Vector3.right * 0.3f;
+        Vector3 EP = transform.position - Vector3.up * 0.1f;
+        Debug.DrawLine(left_SP, EP);
+        Debug.DrawLine(right_SP, EP);
     }
 
     private void FixedUpdate()
     {
-        //接地判定を得る
-        isGround = ground.IsGround();
-        isHead = head.IsGround();
-
-        //各種座標軸の速度を求める
-        float xSpeed = GetXSpeed();
-        float ySpeed = GetYSpeed();
-
-        //アニメーションを適用
-        SetAnimation();
-
-        //移動速度を設定
-        rb.velocity = new Vector2(xSpeed, ySpeed);
-    }
-
-    /// <summary>
-    /// Y成分で必要な計算をし、速度を返す
-    /// </summary>
-    /// <return>Y軸の速さ</return>
-    private float GetYSpeed()
-    {
-        float jumpKey = Input.GetAxisRaw("Jump");
-        float ySpeed = -gravity;
-
-        if (isGround)
+        //待機
+        if (x_val == 0)
         {
-            if (jumpKey > 0)
-            {
-                ySpeed = jumpSpeed;
-                jumpPos = transform.position.y; //ジャンプした位置を記録する
-                isJump = true;
-                jumpTime = 0.0f;
-            }
-            else
-            {
-                isJump = false;
-            }
+            speed = 0f;
         }
-        else if (isJump)
+        //右に移動
+        else if (x_val > 0)
         {
-            //ジャンプキーを押しているか
-            bool pushUpKey = jumpKey > 0;
-            //現在の高さが跳べる高さより下か
-            bool canHeight = jumpPos + jumpHeight > transform.position.y;
-            //ジャンプ時間が長くなりすぎてないか
-            bool canTime = jumpLimitTime > jumpTime;
-
-            if (pushUpKey && canHeight && canTime && !isHead)
-            {
-                ySpeed = jumpSpeed;
-                jumpTime += Time.deltaTime;
-            }
-            else
-            {
-                isJump = false;
-                jumpTime = 0.0f;
-            }
-        }
-
-        if (isJump)
-        {
-            ySpeed *= jumpCurve.Evaluate(jumpTime);
-        }
-
-        return ySpeed;
-    }
-
-    /// <summary>
-    /// X成分で必要な計算をし、速度を返す
-    /// </summary>
-    /// <return>X軸の速さ</return>
-    private float GetXSpeed()
-    {
-        float horizontalKey = Input.GetAxis("Horizontal");
-        float xSpeed = 0.0f;
-
-        if (horizontalKey > 0)
-        {
+            speed = inputSpeed;
             transform.localScale = new Vector3(1, 1, 1);
-            anim.SetBool("run", true);
-            dashTime += Time.deltaTime;
-            xSpeed = speed;
         }
-        else if (horizontalKey < 0)
+        //左に移動
+        else if (x_val < 0)
         {
+            speed = inputSpeed * -1;
             transform.localScale = new Vector3(-1, 1, 1);
-            anim.SetBool("run", true);
-            dashTime += Time.deltaTime;
-            xSpeed = -speed;
-        }
-        else
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-            anim.SetBool("run", false);
-            xSpeed = 0.0f;
-            dashTime = 0.0f;
         }
 
-        //前回の入力からダッシュの反転を判断して速度を変える
-        if (horizontalKey > 0 && beforeKey < 0)
-        {
-            dashTime = 0.0f;
-        }
-        else if (horizontalKey < 0 && beforeKey > 0)
-        {
-            dashTime = 0.0f;
-        }
-
-        beforeKey = horizontalKey;
-        xSpeed *= dashCurve.Evaluate(dashTime);
-        beforeKey = horizontalKey;
-        return xSpeed;
+        //キャラクターを移動
+        rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
     }
-
-    ///<summary>
-    /// アニメーションを設定する
-    ///</summary>
-    private void SetAnimation()
-    {
-        anim.SetBool("jump", isJump);
-        anim.SetBool("ground", isGround);
-        anim.SetBool("run", isRun);
-    }
-
 }
