162ac5a504d68cb2cea417ea13ac172efc3939f7
yndyk <suivivor0519@gmail.com>
2021-05-11 11:03:48 +0900

    //追加しました。

---
+++ Unity/前期制作/Assets/Sprect/OnOffControll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffControll : MonoBehaviour
+{
+    [SerializeField] GameObject Object;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey("up")) 
+        {
+            Object.SetActive(true);
+        }
+        else 
+        {
+            Object.SetActive(false);
+        }
+    } 
+}
