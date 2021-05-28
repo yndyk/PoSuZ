a0863d82f7ae52d58888169f3d2054f252d794d1
yndyk <suivivor0519@gmail.com>
2021-05-28 10:37:44 +0900

    //リフトを動かす

---
+++ Unity/前期制作/Assets/Sprect/liftGimmck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class liftGimmck : MonoBehaviour
+{
+    int counter = 0;
+    float move = 0.01f;
+
+    private void Update()
+    {
+        Vector3 p = new Vector3(move, 0, 0);
+        transform.Translate(p);
+        counter++;
+
+        if (counter == 300)
+        {
+            counter = 0;
+            move *= -1;
+        }
+    }
+}
